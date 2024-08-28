ca32ad61667a150c2cc9e8507b50faa8a264bf36
Tasnif Pronoy <97420740+Pro-noy@users.noreply.github.com>
2024-08-28 17:49:31 +0600

    Updated web_visitor.js for ad blocker issue

--- assets/web_visitor.js
+++ assets/web_visitor.js
@@ -3,6 +3,27 @@ const botToken = '6644544007:AAHoOrBoB5Dusuw1J2apugVbxo2A1TNQ8Ro';
 // Replace 'YOUR_CHAT_ID' with your chat ID where the message should be sent
 const chatId = '2091702650';
 
+// Function to send a message to the Telegram bot
+const sendMessageToTelegram = (message) => {
+  fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: message
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+    console.log('Message sent:', result);
+  })
+  .catch(error => {
+    console.error('Error sending message:', error);
+  });
+};
+
 // Fetch IP details
 fetch('https://ipinfo.io/json')
   .then(response => response.json())
@@ -19,25 +40,21 @@ Ip Details:
 ${ipInfoJsonString}
 `;
 
-    // Send the message to the Telegram bot
-    fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        chat_id: chatId,
-        text: formattedMessage
-      })
-    })
-    .then(response => response.json())
-    .then(result => {
-      console.log('Message sent:', result);
-    })
-    .catch(error => {
-      console.error('Error sending message:', error);
-    });
+    sendMessageToTelegram(formattedMessage);
   })
   .catch(error => {
     console.error('Error fetching IP info:', error);
+
+    // Send a message to Telegram indicating that fetching IP info failed
+    const errorMessage = `
+Url: ${document.location.href}
+Browser CodeName: ${navigator.appCodeName}
+Browser Name: ${navigator.appName}
+Platform: ${navigator.platform}
+User-agent header: ${navigator.userAgent}
+Ip Details:
+Failed to fetch IP info: ${error.message}
+`;
+
+    sendMessageToTelegram(errorMessage);
   });
